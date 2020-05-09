adc5df1f9ad2e0441ab00e5c9b33d3d4c372bc74
Nabin Sayeed <saayeednabin@gmail.com>
2020-05-09 22:30:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+var x = 9; // expression
+var y = 3;
+
+function App() {
+  return (
+    <div className="App">
+{x+y}
+    </div>
+  );
+}
+
+export default App;
